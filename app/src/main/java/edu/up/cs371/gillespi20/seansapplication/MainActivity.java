d1dcb23ea8d1cfae78cbd35f25434357d2289334
gillespi20 <gillespi20@up.edu>
2017-08-31 16:56:30 -0700

    Initial Commit

---
+++ app/src/main/java/edu/up/cs371/gillespi20/seansapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.up.cs371.gillespi20.seansapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
